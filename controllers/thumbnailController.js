3e78282c498d7b9a1498540b65c4d40e2737ea5d
VirendraRaut <virendraraut1306@gmail.com>
2025-12-15 18:10:34 +0530

    thumbnail controller

--- controllers/thumbnailController.js
+++ controllers/thumbnailController.js
@@ -1,3 +1,5 @@
 const Thumbnail = require("../models/thumbnail.js");
 const path = require("path");
 const fs = require("fs");
+const { pipeline } = require("stream");
+const util = require("util");
